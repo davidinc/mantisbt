14626086896e88377ea2081b10d89d75b9712457
Gianluca Sforna <giallu@gmail.com>
2009-05-23 01:40:03 +0200

    Move to new API

--- bug_vote_add.php
+++ bug_vote_add.php
@@ -22,20 +22,18 @@
 # --------------------------------------------------------
 
 require_once( 'core.php' );
-$t_core_path = config_get( 'core_path' );
-require_once( $t_core_path.'current_user_api.php' );
-require_once( $t_core_path.'vote_api.php' );
+require_once( 'current_user_api.php' );
 
-if ( vote_is_enabled() )
+if ( Mantis_Vote::is_enabled() )
 {
-	$f_bug_id		= gpc_get_int( 'bug_id' );
-	$f_weight		= gpc_get_int( 'vote_weight' );
+	$f_bug_id   = gpc_get_int( 'bug_id' );
+	$f_weight   = gpc_get_int( 'vote_weight' );
 	$t_user_id  = auth_get_current_user_id();
 
 	access_ensure_bug_level( config_get( 'voting_place_vote_threshold' ), $f_bug_id, $t_user_id );
 
-	if (!vote_exists($f_bug_id, $t_user_id)){
-		vote_add($f_bug_id, $f_weight, $t_user_id);
+	if ( ! Mantis_Vote::exists($f_bug_id, $t_user_id)){
+		Mantis_Vote::add($f_bug_id, $f_weight, $t_user_id);
 	}
 	print_successful_redirect_to_bug($f_bug_id);
 }
